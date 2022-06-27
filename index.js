0e34c91797837a2b6dab841e9d9a50be667bcff8
Paulo Rogerio <kiko.sjs@gmail.com>
2022-06-26 21:39:38 -0300

    ocn

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = requiere("express");
 const app = express();
 
+const port= process.env.PORT || 3000;
+
 app.get("/", function(req, res){
     res.sendFile(__dirname+"index.html")
 });
 
 
-app.listen(8081);
+app.listen(port, () => {
+    console.info("localhost:3000")
+});
